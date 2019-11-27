9b7a0ef3c5fd8b7d21e9e5d4af46f5a3e8c8244f
wgolanka <wiktoria.golanka@gmail.com>
2019-11-27 07:58:00 +0100

    Add api calls for CRUD operations on tea object

--- app.js
+++ app.js
@@ -9,6 +9,121 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
+app.get('/teatime/tea/all', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'teas retrieved successfully',
+        teas: db
+    })
+});
+
+//----tea controllers ----
+
+function sendBadRequestOnMissingParam(res, paramName) {
+    return res.status(400).send({
+        success: 'false',
+        message: paramName + ' is required'
+    });
+}
+
+app.post('/teatime/tea/add', (req, res) => {
+    if (!req.body.name) { //TODO add more validation, extract to method
+        return sendBadRequestOnMissingParam(res, 'name');
+    } else if (!req.body.description) {
+        return sendBadRequestOnMissingParam(res, 'description')
+    }
+    const tea = {
+        id: uuidv1(),
+        name: req.body.name,
+        originCountry: req.body.originCountry,
+        harvestSeason: req.body.harvestSeason,
+        caffeineContent: req.body.caffeineContent,
+        description: req.body.description,
+        imageLink: req.body.imageLink
+    };
+    db.push(tea);
+    return sendSuccessWithResponse(res, tea, 'tea added successfully');
+});
+
+function sendSuccessWithResponse(res, teas, message) {
+    return res.status(200).send({
+        success: 'true',
+        message: message,
+        teas,
+    });
+}
+
+app.get('/teatime/tea/get/:id', (req, res) => {
+    db.map((teas) => {
+        if (teas.id === req.params.id) {
+            return sendSuccessWithResponse(res, teas, 'teas retried successfully');
+        }
+    });
+    return sendNotFound(res, 'tea')
+});
+
+app.delete('/teatime/tea/delete/:id', (req, res) => {
+    db.map((tea, index) => {
+        if (tea.id === req.params.id) {
+            db.splice(index, 1);
+            return res.status(200).send({
+                success: 'true',
+                message: 'tea deleted successfully',
+            });
+        }
+    });
+    return sendNotFound(res, 'tea')
+});
+
+function sendNotFound(res, name) {
+    return res.status(404).send({
+        success: 'false',
+        message: name + ' not found',
+    });
+}
+
+app.put('/teatime/tea/update/:id', (req, res) => {
+    let teaFound;
+    let itemIndex;
+    db.map((tea, index) => {
+        if (tea.id === req.params.id) {
+            teaFound = tea;
+            itemIndex = index;
+        }
+    });
+
+    if (!teaFound) {
+        return sendNotFound(res, 'tea');
+    }
+
+    if (!req.body.name) { //add more validation
+        return res.status(400).send({
+            success: 'false',
+            message: 'name is required',
+        });
+    } else if (!req.body.description) {
+        return res.status(400).send({
+            success: 'false',
+            message: 'description is required',
+        });
+    }
+
+    const updatedTodo = {
+        id: teaFound.id,
+        name: req.body.name || teaFound.name,
+        originCountry: req.body.originCountry || teaFound.originCountry,
+        harvestSeason: req.body.harvestSeason || teaFound.harvestSeason,
+        caffeineContent: req.body.caffeineContent || teaFound.caffeineContent,
+        description: req.body.description || teaFound.description,
+        imageLink: req.body.imageLink || teaFound.imageLink
+    };
+
+    db.splice(itemIndex, 1, updatedTodo);
+
+    return sendSuccessWithResponse(res, updatedTodo, 'tea updated successfully')
+});
+
+
 //----/tea controllers ----
 const PORT = 5000;
 
