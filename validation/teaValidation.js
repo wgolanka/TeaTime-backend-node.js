bd8b4cafd4bf4963c18f1dbec1435dcc00ee8249
wgolanka <wiktoria.golanka@gmail.com>
2019-12-07 11:54:55 +0100

    Make const of duplicated strings

--- validation/teaValidation.js
+++ validation/teaValidation.js
@@ -3,22 +3,28 @@ import * as httpResponse from "../response/response";
 const stringType = "string";
 const numberType = "number";
 
+const NAME = 'name';
+const DESCRIPTION = 'description';
+const ORIGIN_COUNTRY = 'originCountry';
+const HARVEST_SEASON = 'harvestSeason';
+const CAFFEINE_CONTENT = 'caffeineContent';
+
 module.exports = {
     isAnyRequiredFieldMissing: function (reqBody, response) {
         if (!reqBody.name) {
-            httpResponse.badRequestOnMissingParam(response, 'name');
+            httpResponse.badRequestOnMissingParam(response, NAME);
             return true;
         } else if (!reqBody.description) {
-            httpResponse.badRequestOnMissingParam(response, 'description');
+            httpResponse.badRequestOnMissingParam(response, DESCRIPTION);
             return true;
         } else if (!reqBody.originCountry) {
-            httpResponse.badRequestOnMissingParam(response, 'originCountry');
+            httpResponse.badRequestOnMissingParam(response, ORIGIN_COUNTRY);
             return true;
         } else if (!reqBody.harvestSeason) {
-            httpResponse.badRequestOnMissingParam(response, 'harvestSeason');
+            httpResponse.badRequestOnMissingParam(response, HARVEST_SEASON);
             return true;
         } else if (!reqBody.caffeineContent) {
-            httpResponse.badRequestOnMissingParam(response, 'caffeineContent');
+            httpResponse.badRequestOnMissingParam(response, CAFFEINE_CONTENT);
             return true;
         }
 
@@ -27,19 +33,19 @@ module.exports = {
 
     isAnyFieldWrongParamType: function (reqBody, response) {
         if (!isString(reqBody.name)) {
-            httpResponse.badRequestOnInvalidParamType(response, 'name', stringType);
+            httpResponse.badRequestOnInvalidParamType(response, NAME, stringType);
             return true;
         } else if (!isString(reqBody.description)) {
-            httpResponse.badRequestOnInvalidParamType(response, 'description', stringType);
+            httpResponse.badRequestOnInvalidParamType(response, DESCRIPTION, stringType);
             return true;
         } else if (!isString(reqBody.originCountry)) {
-            httpResponse.badRequestOnInvalidParamType(response, 'originCountry', stringType);
+            httpResponse.badRequestOnInvalidParamType(response, ORIGIN_COUNTRY, stringType);
             return true;
         } else if (!isString(reqBody.harvestSeason)) {
-            httpResponse.badRequestOnInvalidParamType(response, 'harvestSeason', stringType);
+            httpResponse.badRequestOnInvalidParamType(response, HARVEST_SEASON, stringType);
             return true;
         } else if (!isNumber(reqBody.caffeineContent)) {
-            httpResponse.badRequestOnInvalidParamType(response, 'caffeineContent', numberType);
+            httpResponse.badRequestOnInvalidParamType(response, CAFFEINE_CONTENT, numberType);
             return true;
         }
 
