3688df0c55e6fc0e1fab4256d7e78fabc6a64cae
wgolanka <wiktoria.golanka@gmail.com>
2019-12-07 12:10:36 +0100

    Add 2 stub teas

--- db/teaStorage.js
+++ db/teaStorage.js
@@ -1,5 +1,22 @@
 const teas =  [
-
+    {
+        id: "41dd5fd1-e7f4-42a4-95a4-948cc33008ed",
+        name: "China Bencha",
+        originCountry: "China",
+        harvestSeason: "Autumn",
+        caffeineContent: 40,
+        description: "My fav tea",
+        imageLink: "https://pet-uploads.adoptapet.com/e/f/f/421886165.jpg"
+    },
+    {
+        id: "90f74b1e-0fc1-467d-a6dd-5d863d3c717d",
+        name: "Japan Sencha",
+        originCountry: "Japan",
+        harvestSeason: "Spring",
+        caffeineContent: 25,
+        description: "Best green tea Of my life",
+        imageLink: "https://images.pexels.com/photos/556416/pexels-photo-556416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    },
 ];
 
 export default teas;
